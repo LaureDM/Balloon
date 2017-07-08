d0bf2ad4ac37662d8ef77c8abc4955e3a2eb1e6b
Kenny Deriemaeker <kenny@inthepocket.mobi>
2017-07-08 19:52:01 +0200

    Lock rotation of projected balloon shadow.

---
+++ Assets/Code/Helpers/LockRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockRotation : MonoBehaviour {
+
+	private Quaternion _rotation;
+
+	// Use this for initialization
+	void Start () {
+		_rotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = _rotation;
+	}
+}
